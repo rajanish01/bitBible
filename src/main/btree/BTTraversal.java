9fb0bc4d83f764e76d5fec9f3bcdd135b2e2a533
Rajanish <rajanish.ranjan@acornmachine.io>
2020-11-06 14:36:03 +0530

    binary search tree construction and traversal
    Signed-off-by: Rajanish <rajanish.ranjan@acornmachine.io>

--- src/main/btree/BTTraversal.java
+++ src/main/btree/BTTraversal.java
@@ -1,4 +1,61 @@
 package main.btree;
 
+/**
+ * @author Rajanish
+ * @date 5-Nov-2020
+ */
 public class BTTraversal {
+
+    private static final BinaryTree tree = new BinaryTree();
+
+    public static void inOrder(BinaryTree.Node root) {
+        if (root != null) {
+            inOrder(root.leftChild);
+            System.out.print(root.value + " ");
+            inOrder(root.rightChild);
+        }
+    }
+
+    public static void preOrder(BinaryTree.Node root) {
+        if (root != null) {
+            System.out.print(root.value + " ");
+            preOrder(root.leftChild);
+            preOrder(root.rightChild);
+        }
+    }
+
+    public static void postOrder(BinaryTree.Node root) {
+        if (root != null) {
+            postOrder(root.leftChild);
+            postOrder(root.rightChild);
+            System.out.print(root.value + " ");
+        }
+    }
+
+    public static void levelOrder() {
+
+    }
+
+    public static void main(String[] args) {
+        tree.inserUtil(50);
+        tree.inserUtil(30);
+        tree.inserUtil(20);
+        tree.inserUtil(40);
+        tree.inserUtil(70);
+        tree.inserUtil(60);
+        tree.inserUtil(80);
+
+        System.out.println("Inorder :- ");
+        inOrder(tree.root);
+
+        System.out.println("\nPreorder :- ");
+        preOrder(tree.root);
+
+        System.out.println("\nPostorder :- ");
+        postOrder(tree.root);
+
+        System.out.println("\nLevel order :- ");
+        levelOrder();
+    }
+
 }
--- src/main/btree/BinaryTree.java
+++ src/main/btree/BinaryTree.java
@@ -1,4 +1,51 @@
 package main.btree;
 
+/**
+ * @author Rajanish
+ * @date 5-Nov-2020
+ */
 public class BinaryTree {
+
+    public BinaryTree() {
+    }
+
+    public static class Node {
+
+        public int value;
+
+        public Node rightChild;
+
+        public Node leftChild;
+
+        public Node(int value) {
+            this.value = value;
+            this.rightChild = null;
+            this.leftChild = null;
+        }
+
+    }
+
+    public Node root;
+
+    public void inserUtil(int val) {
+        root = insert(val, root);
+    }
+
+    private Node insert(int val, Node root) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (val > root.value) {
+            root.rightChild = insert(val, root.rightChild);
+        } else if (val < root.value) {
+            root.leftChild = insert(val, root.leftChild);
+        }
+
+        return root;
+    }
+
+    public void delete(int val) {
+
+    }
 }
